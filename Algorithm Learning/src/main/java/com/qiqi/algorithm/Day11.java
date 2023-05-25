d82b85f45c85ba7624d95616083f6376a494a84f
qiqi <2636680145@qq.com>
2023-05-25 22:29:13 +0800

    study13

--- Algorithm Learning/src/main/java/com/qiqi/algorithm/Day11.java
+++ Algorithm Learning/src/main/java/com/qiqi/algorithm/Day11.java
@@ -143,5 +143,25 @@ public class Day11 {
                 return true; // 找到 target 就不再递归了
         return false; // 未找到 target
     }
+    public String oddString(String[] words) {
+        var d = new HashMap<String, List<String>>();
+        for (var s : words) {
+            int m = s.length();
+            var cs = new char[m - 1];
+            for (int i = 0; i < m - 1; ++i) {
+                cs[i] = (char) (s.charAt(i + 1) - s.charAt(i));
+            }
+            var t = String.valueOf(cs);
+            d.computeIfAbsent(t, k -> new ArrayList<>()).add(s);
+        }
+        for (var ss : d.values()) {
+            if (ss.size() == 1) {
+                return ss.get(0);
+            }
+        }
+        return "";
+
+    }
+}
 
 }

1394f10aef05e2e4de7c81bfbb039f609afb646b
qiqi <2636680145@qq.com>
2023-07-20 23:28:43 +0800

    study 54

--- Algorithm Learning/src/main/java/com/qiqi/problem3/Main.java
+++ Algorithm Learning/src/main/java/com/qiqi/problem3/Main.java
@@ -123,6 +123,20 @@ public class Main {
         }
     }
 
+
+    public int maxSubarraySumCircular(int[] A) {
+        int total = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
+
+        for (int a : A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+    }
+
     public static void main(String[] args) {
         System.out.println(robotSim(new int[]{4,-1,4,-2,4}, new int[][]{{2,4}}));
     }

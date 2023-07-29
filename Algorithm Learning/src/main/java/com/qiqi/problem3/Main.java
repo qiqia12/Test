5e8b5b18ea2ecb845bc84d7eae9c12b4d4817b47
qiqi <2636680145@qq.com>
2023-07-29 23:18:32 +0800

    study 61

--- Algorithm Learning/src/main/java/com/qiqi/problem3/Main.java
+++ Algorithm Learning/src/main/java/com/qiqi/problem3/Main.java
@@ -311,5 +311,25 @@ public class Main {
         }
         return ans;
     }
+    class ListNode {
+       int val;
+       ListNode next;
+       ListNode(int x) {
+           val = x;
+           next = null;
+       }
+   }
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) return false;
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(slow.next != null && fast.next!=null && fast.next.next!=null ){
+            if (slow == fast) return true;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return false;
+    }
+
 
 }

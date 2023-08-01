41b7dec4197fca93b87219b95a224763b6cdc26c
qiqi <2636680145@qq.com>
2023-08-01 22:37:11 +0800

    study 64

--- Algorithm Learning/src/main/java/com/qiqi/problem3/Main.java
+++ Algorithm Learning/src/main/java/com/qiqi/problem3/Main.java
@@ -377,5 +377,16 @@ public class Main {
         }
     }
 
+    public int sumOfPower(int[] nums) {
+        final long MOD = (long) 1e9 + 7;
+        Arrays.sort(nums);
+        long ans = 0, s = 0;
+        for (long x : nums) { // x 作为最大值
+            ans = (ans + x * x % MOD * (x + s)) % MOD; // 中间模一次防止溢出
+            s = (s * 2 + x) % MOD; // 递推计算下一个 s
+        }
+        return (int) ans;
+    }
+
 
 }
